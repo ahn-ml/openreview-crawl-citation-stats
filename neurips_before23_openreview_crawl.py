0803376d9af44f81fd9167c27b1dc01cace6aa52
DongyeongKim <dongyeongkim33@gmail.com>
2024-11-20 21:20:28 +0900

    add neurips before 23

---
+++ neurips_before23_openreview_crawl.py
@@ -0,0 +1,49 @@
+import requests
+import openreview
+import pandas as pd
+from tqdm import tqdm
+
+conf_year = input("Put Conference Year: ")
+
+venue_id = f"NeurIPS.cc/{conf_year}/Conference"
+
+client = openreview.Client(baseurl='https://api.openreview.net')
+submissions = client.get_all_notes(content={'venueid': venue_id}, details='directReplies')    
+
+api_key = ...
+url = "https://api.scrapingdog.com/google_scholar"
+
+params = {
+    "api_key": api_key,
+    "query": "",
+}
+
+paper_titles = []
+citation_statistics = []
+
+
+for submission in tqdm(submissions):
+    if "Submitted" in submission.content["venue"]:
+        continue
+    else:
+        paper_title = submission.content["title"]
+        paper_titles.append(paper_title)
+        params["query"] = "intitle:"+paper_title
+        response = requests.get(url, params=params)
+        while(response.status_code != 200):
+            response = requests.get(url, params=params)
+        data = response.json()
+        if data['scholar_results'][0]['inline_links']['cited_by']['total'] == 'Related articles':
+            citation_statistics.append(0)
+        else:
+            _, _, val = data['scholar_results'][0]['inline_links']['cited_by']['total'].split(" ")
+            citation_statistics.append(int(val))
+
+
+
+stats = {'name': paper_titles, 'num_citation': citation_statistics}
+     
+df = pd.DataFrame(stats)
+df = df.sort_values(by=['num_citation'], ascending=False)
+
+df.to_csv(f'NeurIPS-{conf_year}-citation_stats.csv')
